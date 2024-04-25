3e8646c248d825ef3939459a2110a0a9e5834be4
nakazono1011 <k.nakazono1011@gmail.com>
2024-04-26 01:18:17 +0900

    Fix item_list CSS selector in DiscountCrawler

--- app/crawler/mercari/discount_crawler.py
+++ app/crawler/mercari/discount_crawler.py
@@ -23,7 +23,7 @@ class DiscountCrawler(BaseCrawler):
         listed_item_element = self.driver.find_element(
             By.CSS_SELECTOR, "[data-testid='listed-item-list']"
         )
-        item_list = listed_item_element.find_elements(By.CSS_SELECTOR, "[data-testid='merListItem-container']")
+        item_list = listed_item_element.find_elements(By.CSS_SELECTOR, "[data-testid='listed-item']")
 
         # 要素内の値引き対象のURLを取得
         item_urls = []
